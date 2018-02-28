01459384c51b6ae7700de2d6cc0948c9223941dd
张迪 <codeway3@126.com>
2018-02-28 15:23:59 +0800

    add 111

---
+++ 111_Minimum_Depth_of_Binary_Tree/Minimum_Depth_of_Binary_Tree.py
@@ -0,0 +1,30 @@
+"""
+https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def getminDepth(rt):
+            if not rt:
+                return 0
+            # 注意这里 要考虑到 题目要求的是到【叶子节点】的距离为深度
+            if not rt.left or not rt.right:
+                return getminDepth(rt.left)+getminDepth(rt.right)+1
+            return min(getminDepth(rt.left),getminDepth(rt.right))+1
+        return getminDepth(root)
+
+
+# others solutions
+# 大概都是这个思路 要细心考虑所有可能的情况
